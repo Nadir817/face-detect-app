956c1cabc4ddb5acac6ab32d38c2782024fd8d0d
Nadir817 <nadir.erkin@gmail.com>
2020-01-01 19:57:57 -0500

    Updates

---
+++ precache-manifest.7c4d4e1fd5a42419e33f75ac73bc7e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26842354cd8df75aa868bb32f116cc2d",
+    "url": "/face-detect-app/index.html"
+  },
+  {
+    "revision": "3d197bc8105aa19213cf",
+    "url": "/face-detect-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "42d20f08143ef9150468",
+    "url": "/face-detect-app/static/css/main.3a284464.chunk.css"
+  },
+  {
+    "revision": "3d197bc8105aa19213cf",
+    "url": "/face-detect-app/static/js/2.c0c91c5b.chunk.js"
+  },
+  {
+    "revision": "42d20f08143ef9150468",
+    "url": "/face-detect-app/static/js/main.fec51b85.chunk.js"
+  },
+  {
+    "revision": "c33cf09e820e3d02af03",
+    "url": "/face-detect-app/static/js/runtime-main.ec8e9b9f.js"
+  },
+  {
+    "revision": "989b00645077a0d258006f04b1c42500",
+    "url": "/face-detect-app/static/media/Olympic.989b0064.woff"
+  }
+]);
